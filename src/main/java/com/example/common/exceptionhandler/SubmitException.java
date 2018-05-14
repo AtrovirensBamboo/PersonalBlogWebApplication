62a133bd9bf79118e42f40eae869909aac1b321a
AtrovirensBamboo <37063000+AtrovirensBamboo@users.noreply.github.com>
2018-05-14 17:15:37 +0800

    该版本新增log4j2的配置

--- src/main/java/com/example/common/exceptionhandler/SubmitException.java
+++ src/main/java/com/example/common/exceptionhandler/SubmitException.java
@@ -1,5 +1,6 @@
 package com.example.common.exceptionhandler;
 
+//!!!!!!注意，若是修改包名，编译后的路径包名可能会出现不更改的问题，就会导致ClassNotFound的错误!!!!!!!
 //自定义异常
 public class SubmitException extends Exception {
     private static final long serialVersionUID = 1L;
